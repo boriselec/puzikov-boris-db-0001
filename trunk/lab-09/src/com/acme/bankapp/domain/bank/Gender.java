e3ed88b780218d566c8a9863b2d1763ec12d3176
boris.puzikov@gmail.com <boris.puzikov@gmail.com@d263337d-73ab-b3b4-c2c8-dd7dca886a48>
2013-04-17 03:33:29 +0000

    fix gender

--- trunk/lab-09/src/com/acme/bankapp/domain/bank/Gender.java
+++ trunk/lab-09/src/com/acme/bankapp/domain/bank/Gender.java
@@ -1,11 +1,15 @@
 package com.acme.bankapp.domain.bank;
 
 public enum Gender {
-	MALE,
-	FEMALE;
+	MALE("Mr. "),
+	FEMALE("Ms. ");
+	
+	private String salut;
+	private Gender(String salut){
+		this.salut = salut;
+	}
 	@Override
 	public String toString() {
-		String result = (this.name() == "MALE") ? "Mr. " : "Ms. ";
-		return result;
+		return salut;
 	}
 }
