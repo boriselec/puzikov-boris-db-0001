88cff9b825e7e9543906251f0654f26be7e9c7df
boris.puzikov <boris.puzikov@gmail.com>
2013-04-17 07:27:00 +0000

    lab-11 done

--- lab-11/src/com/acme/bankapp/service/bank/BankApplication.java
+++ lab-11/src/com/acme/bankapp/service/bank/BankApplication.java
@@ -1,50 +1,55 @@
 package com.acme.bankapp.service.bank;
+
 import java.util.Date;
 
 import com.acme.bankapp.domain.bank.*;
 
 public class BankApplication {
 	public static void main(String[] args) {
-		BankService service = new BankService();
-		Bank bank = new Bank(
-			new ClientRegistrationListener() {
+		try {
+			BankService service = new BankService();
+			Bank bank = new Bank(new ClientRegistrationListener() {
 
-			@Override
-			public void onClientAdded(Client client) {
-				System.out.println(client);
+				@Override
+				public void onClientAdded(Client client) {
+					System.out.println(client);
 
-			}
-		},
-			new ClientRegistrationListener() {
+				}
+			}, new ClientRegistrationListener() {
 
-			@Override
-			public void onClientAdded(Client client) {
-				System.out.format("Notification for client %s to be sent\n", client);
+				@Override
+				public void onClientAdded(Client client) {
+					System.out.format(
+							"Notification for client %s to be sent\n", client);
 
-			}
-		},
-			new ClientRegistrationListener() {
+				}
+			}, new ClientRegistrationListener() {
 
-			@Override
-			public void onClientAdded(Client client) {
-				System.out.format("Client: %s, Date: %s\n", client, new Date());
+				@Override
+				public void onClientAdded(Client client) {
+					System.out.format("Client: %s, Date: %s\n", client,
+							new Date());
 
-			}
+				}
+			});
+			service.printBalance(bank);
+
+			service.addClient(bank, new Client("Bob", Gender.MALE,
+					new CheckingAccount(2.0, 3.0)));
+			service.printBalance(bank);
+
+			service.addClient(bank, new Client("Alice", Gender.FEMALE,
+					new SavingAccount(4.0)));
+			service.printBalance(bank);
+
+			service.modifyBank(bank);
+			service.printBalance(bank);
+
+			service.printMaximumAmountToWithdraw(bank);
+		} catch (BankException ex) {
+			System.out.println(ex.toString());
 		}
-			);
-		service.printBalance(bank);
-		
-		service.addClient(bank, new Client("Bob", Gender.MALE, new CheckingAccount(2.0, 3.0)));
-		service.printBalance(bank);
-		
-		service.addClient(bank, new Client("Alice", Gender.FEMALE, new SavingAccount(4.0)));
-		service.printBalance(bank);
-		
-		service.modifyBank(bank);
-		service.printBalance(bank);
-		
-		service.printMaximumAmountToWithdraw(bank);
-		
+
 	}
 
 }
--- lab-11/src/com/acme/bankapp/service/bank/BankService.java
+++ lab-11/src/com/acme/bankapp/service/bank/BankService.java
@@ -2,10 +2,13 @@ package com.acme.bankapp.service.bank;
 
 import com.acme.bankapp.domain.bank.Bank;
 import com.acme.bankapp.domain.bank.Client;
+import com.acme.bankapp.domain.bank.ClientExistsException;
+import com.acme.bankapp.domain.bank.NotEnoughFundsException;
+import com.acme.bankapp.domain.bank.OverDraftLimitExceededException;
 
 public class BankService {
 
-	public void addClient(Bank bank, Client client){
+	public void addClient(Bank bank, Client client) throws ClientExistsException{
 		bank.addClient(client);
 		;
 	}
@@ -28,13 +31,18 @@ public class BankService {
 			toPrint += "\n";
 		System.out.println(toPrint);
 	}
-	public void modifyBank(Bank bank){
+	public void modifyBank(Bank bank) throws NotEnoughFundsException{
 		Client[] clients = bank.getClients();
 		if (clients[0] != null){
 			clients[0].getAccounts().deposit(3.0);
 		}
 		if (clients[1] != null){
-			clients[1].getAccounts().withdraw(3.0);
+			try{
+				clients[1].getAccounts().withdraw(3.0);
+			}
+			catch(OverDraftLimitExceededException e){
+				throw new OverDraftLimitExceededException(clients[1], e);
+			}
 		}
 	}
 	
