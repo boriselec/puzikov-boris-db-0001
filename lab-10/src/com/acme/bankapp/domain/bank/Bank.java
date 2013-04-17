24ab960ea49cdb8c96d1a40f41888439457571d9
boris.puzikov <boris.puzikov@gmail.com>
2013-04-17 04:08:24 +0000

    lab-10(part 1) done

--- lab-10/src/com/acme/bankapp/domain/bank/Bank.java
+++ lab-10/src/com/acme/bankapp/domain/bank/Bank.java
@@ -1,10 +1,19 @@
 package com.acme.bankapp.domain.bank;
 
+import com.acme.bankapp.service.bank.ClientRegistrationListener;
+
 
 public class Bank {
 	public final int MAX_CLIENTS = 10;
+	public final int MAX_LISTENERS = 10;
 	private Client[] clients = new Client[MAX_CLIENTS];
+	private ClientRegistrationListener[] listeners = new ClientRegistrationListener[MAX_LISTENERS];
 	private int numOfClients = 0;
+	private int numOfListeners = 0;
+	
+	public Bank(ClientRegistrationListener... listeners){
+		this.listeners = listeners;
+	}
 	
 	public int getNumOfClients() {
 		return numOfClients;
@@ -20,6 +29,21 @@ public class Bank {
 		}
 		else {
 			clients[numOfClients++] = client;
+			if (listeners != null)
+			for (ClientRegistrationListener listener : listeners){
+				if (listener != null)
+					listener.onClientAdded(client);
+			}
+		}
+		
+	}
+	
+	public void addListener(ClientRegistrationListener listener){
+		if (numOfListeners >= MAX_LISTENERS){
+			//throw
+		}
+		else {
+			listeners[numOfListeners++] = listener;
 		}
 		
 	}
--- lab-10/src/com/acme/bankapp/service/bank/BankApplication.java
+++ lab-10/src/com/acme/bankapp/service/bank/BankApplication.java
@@ -1,10 +1,37 @@
 package com.acme.bankapp.service.bank;
+import java.util.Date;
+
 import com.acme.bankapp.domain.bank.*;
 
 public class BankApplication {
 	public static void main(String[] args) {
 		BankService service = new BankService();
-		Bank bank = new Bank();
+		Bank bank = new Bank(
+			new ClientRegistrationListener() {
+
+			@Override
+			public void onClientAdded(Client client) {
+				System.out.println(client);
+
+			}
+		},
+			new ClientRegistrationListener() {
+
+			@Override
+			public void onClientAdded(Client client) {
+				System.out.format("Notification for client %s to be sent\n", client);
+
+			}
+		},
+			new ClientRegistrationListener() {
+
+			@Override
+			public void onClientAdded(Client client) {
+				System.out.format("Client: %s, Date: %s\n", client, new Date());
+
+			}
+		}
+			);
 		service.printBalance(bank);
 		
 		service.addClient(bank, new Client("Bob", Gender.MALE, new CheckingAccount(2.0, 3.0)));
