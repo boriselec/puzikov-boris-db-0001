7c8b92d34b6c5b84472fbfff076a37abe7c74c59
boris.puzikov@gmail.com <boris.puzikov@gmail.com@d263337d-73ab-b3b4-c2c8-dd7dca886a48>
2013-04-17 04:08:45 +0000

    fix

--- trunk/lab-09/src/com/acme/bankapp/service/bank/ClientRegistrationListener.java
+++
@@ -1,8 +0,0 @@
-package com.acme.bankapp.service.bank;
-
-import com.acme.bankapp.domain.bank.Client;;
-
-
-public interface ClientRegistrationListener {
-	public void onClientAdded(Client client);
-}
