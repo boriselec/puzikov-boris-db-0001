c1b200391e8f891b6d87c6662473d6bed045728f
boris.puzikov@gmail.com <boris.puzikov@gmail.com@d263337d-73ab-b3b4-c2c8-dd7dca886a48>
2013-04-19 19:41:48 +0000

    lab-08 task2 fix

--- trunk/lab-08/src/com/acme/task2/GCTest.java
+++ trunk/lab-08/src/com/acme/task2/GCTest.java
@@ -2,10 +2,14 @@ package com.acme.task2;
 
 public class GCTest {
 
+	private final static int ARRAY_SIZE = 1000000;
+	
 	public static void main(String[] args) {
-		int numberOfIterations = 1000000;
-		
-		//gc don't work because of array
+		testGC();
+	}
+	
+	public static void testGC() {
+		int numberOfIterations = ARRAY_SIZE;
 		Foo[] array = new Foo[numberOfIterations];
 		
 		for (int i = 0; i < numberOfIterations; i++) {
