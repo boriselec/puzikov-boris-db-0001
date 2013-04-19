1784d8b8ff97933740d7854d90fcc959a7d0ca3e
boris.puzikov@gmail.com <boris.puzikov@gmail.com@d263337d-73ab-b3b4-c2c8-dd7dca886a48>
2013-04-19 19:59:56 +0000

    lab-08 task5 fix

--- trunk/lab-08/src/com/acme/task5/TextModifier.java
+++ trunk/lab-08/src/com/acme/task5/TextModifier.java
@@ -1,12 +1,26 @@
 package com.acme.task5;
 
 public class TextModifier {
+	
+	private static final String ERR_ARGS_NUMBER = 
+			"Wrong number of arguments. Must be at least 1.";
+	
 	public static void main(String[] args) {
+		
+		testModifier(args);
+		
+	}
+	
+	public static void testModifier(String[] args) {
 		if (args.length == 0){
-			System.out.println("Wrong args");
-			return;
+			throw new IllegalArgumentException(ERR_ARGS_NUMBER);
 		}
-		String str = args[0];
+		modify(args[0]);
+		
+	}
+	
+	public static void modify(String str) {
+		
 		for (int i = 0; i < str.length(); i++) {
 			char charAtIndex = str.charAt(i);
 			if (Character.isDigit(charAtIndex) == false){
