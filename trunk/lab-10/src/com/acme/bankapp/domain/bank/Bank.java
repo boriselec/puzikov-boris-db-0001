53b52789246b9be7542836fea7b0f66a81f5584c
boris.puzikov@gmail.com <boris.puzikov@gmail.com@d263337d-73ab-b3b4-c2c8-dd7dca886a48>
2013-04-19 21:13:46 +0000

    recreate to lab-09

--- trunk/lab-10/src/com/acme/bankapp/domain/bank/Bank.java
+++ trunk/lab-10/src/com/acme/bankapp/domain/bank/Bank.java
@@ -1,19 +1,10 @@
 package com.acme.bankapp.domain.bank;
 
-import com.acme.bankapp.service.bank.ClientRegistrationListener;
-
 
 public class Bank {
 	public final int MAX_CLIENTS = 10;
-	public final int MAX_LISTENERS = 10;
 	private Client[] clients = new Client[MAX_CLIENTS];
-	private ClientRegistrationListener[] listeners = new ClientRegistrationListener[MAX_LISTENERS];
 	private int numOfClients = 0;
-	private int numOfListeners = 0;
-	
-	public Bank(ClientRegistrationListener... listeners){
-		this.listeners = listeners;
-	}
 	
 	public int getNumOfClients() {
 		return numOfClients;
@@ -24,26 +15,8 @@ public class Bank {
 	}
 	
 	public void addClient(Client client){
-		if (numOfClients >= MAX_CLIENTS){
-			//throw
-		}
-		else {
+		if (numOfClients < MAX_CLIENTS){
 			clients[numOfClients++] = client;
-			if (listeners != null)
-			for (ClientRegistrationListener listener : listeners){
-				if (listener != null)
-					listener.onClientAdded(client);
-			}
-		}
-		
-	}
-	
-	public void addListener(ClientRegistrationListener listener){
-		if (numOfListeners >= MAX_LISTENERS){
-			//throw
-		}
-		else {
-			listeners[numOfListeners++] = listener;
 		}
 		
 	}
