00930b5fe3498329f232461790decf387e894c64
boris.puzikov <boris.puzikov@gmail.com>
2013-04-27 14:02:34 +0000

    lab-15/16 done

--- lab-15/src/com/acme/domain/email/impl/QueueImpl.java
+++ lab-15/src/com/acme/domain/email/impl/QueueImpl.java
@@ -1,57 +1,64 @@
 package com.acme.domain.email.impl;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-
-import com.acme.domain.email.Email;
-import com.acme.domain.email.Queue;
 
 public class QueueImpl<K> {
 	private final int MAX_ELELMENT;
 	private final Object monitor = new Object();
+	private List<K> mails = new ArrayList<K>();
+	private boolean isClose = false;
 
 	public QueueImpl(int max) {
 		MAX_ELELMENT = max;
 	}
-	
+
 	public int getSize() {
 		return mails.size();
 	}
-	
-	private List<K> mails = new ArrayList<K>();
 
 	public void addEmail(K email) {
-			synchronized (monitor) {
-				
-				while (mails.size() == MAX_ELELMENT) {
-					try {
-						monitor.wait();
-					} catch (InterruptedException e) {
-						return;
-					}
+		synchronized (monitor) {
+
+			while (mails.size() == MAX_ELELMENT && (isClose == false)) {
+				try {
+					monitor.wait();
+				} catch (InterruptedException e) {
 				}
+			}
+			if (isClose == true) {
+				throw new IllegalStateException();
+
+			} else {
 				monitor.notify();
 				mails.add(email);
 			}
 		}
+	}
 
 	public K getEmail() {
-			synchronized (monitor) {
+		synchronized (monitor) {
 
-			while (mails.isEmpty()) {
+			while (mails.isEmpty() && (isClose == false)) {
 				try {
 					monitor.wait();
 				} catch (InterruptedException e) {
-					return null;
 				}
 			}
-			monitor.notify();
-			return mails.remove(0);
+			if (isClose == false) {
+				monitor.notify();
+				return mails.remove(0);
+			} else {
+				return null;
+			}
 		}
 	}
+
 	public void close() {
-		notify();
+		synchronized (monitor) {
+			isClose = true;
+			monitor.notifyAll();
+		}
 	}
-	
+
 }
