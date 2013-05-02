226deda62df13a3735c43f69ddad7ac19b3f7774
boris.puzikov <boris.puzikov@gmail.com>
2013-05-02 16:07:43 +0000

    Added test cover for offerbook. Fixed bugs. Added splice for reports with same orderID.

--- stock-exchange-emulator/src/main/java/com/stockexchangeemulator/domain/Response.java
+++ stock-exchange-emulator/src/main/java/com/stockexchangeemulator/domain/Response.java
@@ -1,17 +1,29 @@
 package com.stockexchangeemulator.domain;
 
+import java.util.Date;
+
 public class Response {
 
 	public Response(WrappedOrder wrappedOrder, Status status, float price,
-			int sharesCount) {
-		// TODO Auto-generated constructor stub
+			int sharesCount, Date date) {
+		this.clientID = wrappedOrder.getClientID();
+		this.status = status;
+		this.price = price;
+		this.tradedShares = sharesCount;
+		this.requestedShares = wrappedOrder.getOrder().getSharesCount();
+		this.message = "Ok";
+		this.date = date;
+		this.orderID = wrappedOrder.getOrderID();
 	}
 
 	private int clientID;
 	private Status status;
 	private float price;
 	private int tradedShares;
+	private int requestedShares;
 	private String message;
+	private Date date;
+	private int orderID;
 
 	public Status getStatus() {
 		return status;
@@ -32,4 +44,20 @@ public class Response {
 	public int getClientID() {
 		return clientID;
 	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public int getOrderID() {
+		return orderID;
+	}
+
+	public Response splice(Response response2) {
+		this.tradedShares += response2.tradedShares;
+		if (response2.status == Status.FULLY_FILLED)
+			this.status = Status.FULLY_FILLED;
+		return this;
+	}
+
 }
