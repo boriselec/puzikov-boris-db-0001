07c9f5774a65e0f0cad64ab14b62286c65537985
boris.puzikov@gmail.com <boris.puzikov@gmail.com@d263337d-73ab-b3b4-c2c8-dd7dca886a48>
2013-05-02 12:35:03 +0000

    added price matchers

---
+++ trunk/stock-exchange-emulator/src/main/java/com/stockexchangeemulator/domain/ConstantPriceMatcher.java
@@ -0,0 +1,16 @@
+package com.stockexchangeemulator.domain;
+
+public class ConstantPriceMatcher implements PriceMatcher {
+
+	private float matchingPrice;
+
+	public ConstantPriceMatcher(float matchingPrice) {
+		this.matchingPrice = matchingPrice;
+	}
+
+	@Override
+	public float match(WrappedOrder o1, WrappedOrder o2) {
+		return matchingPrice;
+	}
+
+}
--- trunk/stock-exchange-emulator/src/main/java/com/stockexchangeemulator/stockexchange/business/OrderBook.java
+++ trunk/stock-exchange-emulator/src/main/java/com/stockexchangeemulator/stockexchange/business/OrderBook.java
@@ -5,9 +5,12 @@ import java.util.LinkedList;
 import java.util.TreeSet;
 import java.util.logging.Logger;
 
+import com.stockexchangeemulator.domain.ConstantPriceMatcher;
 import com.stockexchangeemulator.domain.InverseOrderComparator;
+import com.stockexchangeemulator.domain.LastPriceMatcher;
 import com.stockexchangeemulator.domain.OrderComparator;
 import com.stockexchangeemulator.domain.PriceComparator;
+import com.stockexchangeemulator.domain.PriceMatcher;
 import com.stockexchangeemulator.domain.Response;
 import com.stockexchangeemulator.domain.Status;
 import com.stockexchangeemulator.domain.WrappedOrder;
@@ -18,14 +21,21 @@ public class OrderBook {
 	public OrderBook() {
 		bidsOrderBook = new TreeSet<>(new InverseOrderComparator());
 		offersOrderBook = new TreeSet<>(new OrderComparator());
-		spreadMax = Float.POSITIVE_INFINITY;
-		spreadMin = Float.NEGATIVE_INFINITY;
+
+		this.startupMatcher = new ConstantPriceMatcher(getAveragePrice());
+		this.mainMatcher = new LastPriceMatcher();
 	}
 
-	TreeSet<WrappedOrder> bidsOrderBook;
-	TreeSet<WrappedOrder> offersOrderBook;
-	float spreadMax;
-	float spreadMin;
+	private float getAveragePrice() {
+		// TODO: return average price for all crossing orders
+		return (float) 0.0;
+	}
+
+	private TreeSet<WrappedOrder> bidsOrderBook;
+	private TreeSet<WrappedOrder> offersOrderBook;
+
+	private PriceMatcher mainMatcher;
+	private PriceMatcher startupMatcher;
 
 	public LinkedList<Response> proceedOrder(WrappedOrder wrappedOrder) {
 		addOrder(wrappedOrder);
@@ -78,15 +88,18 @@ public class OrderBook {
 			int offerSharesCount = offer.getOrder().getSharesCount();
 
 			if (PriceComparator.match(offerPrice, bidPrice)) {
+				float dealPrice = mainMatcher.match(offer, bid);
 				if (offerSharesCount > bidSharesCount) {
-					responses.add(fullyFill(bid));
-					responses.add(partiallyFill(offer, bidSharesCount));
+					responses.add(fullyFill(bid, dealPrice));
+					responses.add(partiallyFill(offer, dealPrice,
+							bidSharesCount));
 				} else if (offerSharesCount < bidSharesCount) {
-					responses.add(partiallyFill(bid, offerSharesCount));
-					responses.add(fullyFill(offer));
+					responses.add(partiallyFill(bid, dealPrice,
+							offerSharesCount));
+					responses.add(fullyFill(offer, dealPrice));
 				} else {
-					responses.add(fullyFill(bid));
-					responses.add(fullyFill(offer));
+					responses.add(fullyFill(bid, dealPrice));
+					responses.add(fullyFill(offer, dealPrice));
 				}
 			} else
 				break;
@@ -94,125 +107,22 @@ public class OrderBook {
 		return responses;
 	}
 
-	private Response partiallyFill(WrappedOrder wrappedOrder, int sharesCount) {
+	private Response partiallyFill(WrappedOrder wrappedOrder, float price,
+			int sharesCount) {
 		Response response = new Response(wrappedOrder, Status.PARTIALLY_FILLED,
-				sharesCount);
+				price, sharesCount);
 		wrappedOrder.getOrder().partlyFill(sharesCount);
 		log.info("partially filled");
 		return response;
 	}
 
-	private Response fullyFill(WrappedOrder wrappedOrder) {
+	private Response fullyFill(WrappedOrder wrappedOrder, float price) {
 		removeOrder(wrappedOrder);
 		int sharesCount = wrappedOrder.getOrder().getSharesCount();
 		Response response = new Response(wrappedOrder, Status.FULLY_FILLED,
-				sharesCount);
+				price, sharesCount);
 		log.info("fully filled");
 		return response;
 	}
 
-	// public LinkedList<Response> proceedOrderOld(WrappedOrder wrappedOrder) {
-	//
-	// Order order = wrappedOrder.getOrder();
-	// Type type = order.getType();
-	//
-	// boolean isInSpread = order.getPrice() > spreadMin
-	// && order.getPrice() < spreadMax;
-	// boolean isProceed = (order.getPrice() <= spreadMin && type == Type.OFFER)
-	// || (order.getPrice() <= spreadMax && type == Type.BID);
-	//
-	// switch (type) {
-	//
-	// case BID:
-	// if (isInSpread == true) {
-	// bidsOrderBook.add(wrappedOrder);
-	// spreadMin = order.getPrice();
-	// log.info("New bid added");
-	// return null;
-	// } else if (isProceed == true) {
-	// return fillOrderOld(wrappedOrder, Type.BID);
-	//
-	// } else {
-	// bidsOrderBook.add(wrappedOrder);
-	// log.info("New bid added");
-	// return null;
-	// }
-	//
-	// case OFFER:
-	// if (isInSpread == true) {
-	// offersOrderBook.add(wrappedOrder);
-	// spreadMax = order.getPrice();
-	// log.info("New offer added");
-	// return null;
-	// } else if (isProceed == true) {
-	// return fillOrderOld(wrappedOrder, Type.OFFER);
-	//
-	// } else {
-	// offersOrderBook.add(wrappedOrder);
-	// log.info("New offer added");
-	// return null;
-	// }
-	// }
-	// return null;
-	// }
-	//
-	// private LinkedList<Response> fillOrderOld(WrappedOrder wrappedOrder,
-	// Type type) {
-	//
-	// TreeSet<WrappedOrder> proposalBook = (type == Type.BID) ? offersOrderBook
-	// : bidsOrderBook;
-	// TreeSet<WrappedOrder> orderBook = (type == Type.BID) ? bidsOrderBook
-	// : offersOrderBook;
-	//
-	// float orderPrice = wrappedOrder.getOrder().getPrice();
-	// int orderSharesCount = wrappedOrder.getOrder().getSharesCount();
-	//
-	// LinkedList<Response> responses = new LinkedList<>();
-	// Iterator<WrappedOrder> iterator = proposalBook.iterator();
-	//
-	// boolean isFullyFilled = false;
-	// while (iterator.hasNext()) {
-	// WrappedOrder proposalOrder = iterator.next();
-	// float proposalPrice = proposalOrder.getOrder().getPrice();
-	// int proposalSharesCount = proposalOrder.getOrder().getSharesCount();
-	//
-	// boolean isNoMoreOrders = (proposalPrice > orderPrice && type == Type.BID)
-	// || (proposalPrice < orderPrice && type == Type.OFFER);
-	//
-	// if (isNoMoreOrders == true) {
-	// break;
-	// }
-	//
-	// if (orderSharesCount > proposalSharesCount) {
-	// responses.add(new Response(proposalOrder, Status.FULLY_FILLED));
-	// proposalBook.remove(proposalOrder);
-	// wrappedOrder.getOrder().partlyFill(proposalSharesCount);
-	// log.info("Partly filled - Fully filled");
-	//
-	// } else if (orderSharesCount == proposalSharesCount) {
-	// responses.add(new Response(proposalOrder, Status.FULLY_FILLED));
-	// responses.add(new Response(wrappedOrder, Status.FULLY_FILLED));
-	// proposalBook.remove(proposalOrder);
-	// isFullyFilled = true;
-	// log.info("Fully filled - Fully filled");
-	// break;
-	//
-	// } else {
-	// responses.add(new Response(wrappedOrder, Status.FULLY_FILLED));
-	// responses.add(new Response(proposalOrder,
-	// Status.PARTIALLY_FILLED));
-	// proposalOrder.getOrder().partlyFill(orderSharesCount);
-	// isFullyFilled = true;
-	// log.info("Fully filled - Partly filled");
-	// break;
-	// }
-	// }
-	// if (isFullyFilled == false) {
-	// responses.add(new Response(wrappedOrder, Status.PARTIALLY_FILLED));
-	// orderBook.add(wrappedOrder);
-	// log.info("New offer/bid added");
-	// }
-	//
-	// return responses;
-	// }
 }
